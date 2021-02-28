3c38b699eb061f3d2c908742ffcb3277effe3ec4
Pierre Wendling <pierre.wendling.4@gmail.com>
2021-02-28 10:36:15 -0500

    Added doxygen docs to game_rand header

--- src/include/game_rand.h
+++ src/include/game_rand.h
@@ -2,6 +2,15 @@
 #define GAME_RAND_H
 #include "game.h"
 
+/**
+ * @brief Creates a valid game randomly
+ * @param width the width of the board for the game (number of columns)
+ * @param height the height of the board for the game (number of rows)
+ * @param swap a boolean indicating whether the board is "wrapping"
+ * @param allow_cross a boolean indicating whether the board can contain CROSS
+ *pieces
+ * @return the randomly generated game
+ **/
 game random_game_ext(int width, int height, bool swap, bool allow_cross);
 
-#endif
+#endif  // GAME_RAND_H
